2c7ce9134c2d25ca8cde4772e38ee38f306ecc5d
Aditya Kumar <aditya30394@gmail.com>
2019-04-18 16:26:13 -0500

    Adding the code to download the skipthought model files

--- download_skipthought.py
+++ download_skipthought.py
@@ -1,12 +1,26 @@
-import os
+import wget
 
-print ('Downloading Skip-Thought Model ...........')
-os.sysytem('wget http://www.cs.toronto.edu/~rkiros/models/dictionary.txt')
-os.sysytem('wget http://www.cs.toronto.edu/~rkiros/models/utable.npy')
-os.sysytem('wget http://www.cs.toronto.edu/~rkiros/models/btable.npy')
-os.sysytem('wget http://www.cs.toronto.edu/~rkiros/models/uni_skip.npz')
-os.sysytem('wget http://www.cs.toronto.edu/~rkiros/models/uni_skip.npz.pkl')
-os.sysytem('wget http://www.cs.toronto.edu/~rkiros/models/bi_skip.npz')
-os.sysytem('wget http://www.cs.toronto.edu/~rkiros/models/bi_skip.npz.pkl')
+print('Downloading Skip-Thought Model ...........')
+ 
+wget.download('http://www.cs.toronto.edu/~rkiros/models/dictionary.txt')
+print('\ndictionary.txt Downloaded!')
 
-print ('Download Completed ............')
+wget.download('http://www.cs.toronto.edu/~rkiros/models/utable.npy')
+print('\nutable.npy Downloaded!')
+
+wget.download('http://www.cs.toronto.edu/~rkiros/models/btable.npy')
+print('\nbtable.npy Downloaded!')
+
+wget.download('http://www.cs.toronto.edu/~rkiros/models/uni_skip.npz')
+print('\nuni_skip.npz Downloaded!')
+
+wget.download('http://www.cs.toronto.edu/~rkiros/models/uni_skip.npz.pkl')
+print('\nuni_skip.npz.pkl Downloaded!')
+
+wget.download('http://www.cs.toronto.edu/~rkiros/models/bi_skip.npz')
+print('\nbi_skip.npz Downloaded!')
+
+wget.download('http://www.cs.toronto.edu/~rkiros/models/bi_skip.npz.pkl')
+print('\nbi_skip.npz.pkl Downloaded!')
+
+print('Download Completed ............')
